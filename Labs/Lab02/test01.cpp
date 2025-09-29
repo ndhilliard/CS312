6569d4a201ab1ff1c434094fb2f60a6597739773
ndhilliard <n_hilliard@hotmail.com>
2025-09-29 15:39:30 -0400

    Add moving average calculation in test01.cpp
    Implemented main function to calculate and display moving averages for a dataset.

--- Labs/Lab02/test01.cpp
+++ Labs/Lab02/test01.cpp
@@ -6,13 +6,43 @@
 
 using namespace std;
 
+
 int main() {
-	
 
-}
+	int A[] = { 1, 2, 3, 4, 5, 6 };
+	const int n = sizeof(A) / sizeof(A[0]);
+	int s = 3;
+	double T[n];
 
-void printArray(const double A[], double n) {
+	int m = MovingAverage(A, s, n, T);
+
+
+	cout << "Test Case 1 \n";
+	cout << "Dataset [";
+
 	for (int i = 0; i < n; i++) {
-
+		if (i) {
+			cout << ", ";
+		}
+		cout << A[i];
 	}
+
+	cout << "]\n";
+
+	cout << "Moving Average: [";
+
+	for (int i = 0; i < m; i++) {
+		if (i) {
+			cout << ", ";
+		}
+
+		if (T[i] == static_cast<int>(T[i])) {
+			cout << static_cast<int>(T[i]);
+		} else {
+			cout << T[i];
+		}
+	}
+	cout << "]\n";
+
+	return 0;
 }
